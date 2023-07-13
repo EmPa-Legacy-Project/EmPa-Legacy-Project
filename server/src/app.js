d787e93e0b3f542efed86ea3fbbf767bddbe12d2
Emre Guler <mfbg1985@gmail.com>
2023-07-13 23:23:56 +0300

    s

--- server/src/app.js
+++ server/src/app.js
@@ -24,7 +24,7 @@ mongoose
     console.log("Connected!");
   })
   .then(() => {app.listen(5000, () => {
-    console.log("Server is running on port 3001");
+    console.log("Server is running on port 3000");
   });
   }).catch((err)=>{
     console.log(err)
