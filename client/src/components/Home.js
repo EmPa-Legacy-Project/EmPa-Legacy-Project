5299a5d801290f30127cf3749ae9b1a2be36ffd5
Panagiotis-Mantis <png.mantis@gmail.com>
2023-07-16 17:37:25 +0300

    Frontend adaptations

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -4,13 +4,12 @@ import Typography from "@mui/material/Typography";
 import Books from "./Book/Books";
 
 function Home() {
-  const [showBtn, setShowBtn] = useState(true)
-  const handleClick = ()=>{
-    const token = localStorage.getItem("token")
-      setShowBtn(!showBtn)
-      console.log(token)
-   
-  }
+  const [showBtn, setShowBtn] = useState(true);
+  const handleClick = () => {
+    const token = localStorage.getItem("token");
+    setShowBtn(!showBtn);
+    console.log(token);
+  };
 
   return (
     <div>
@@ -18,13 +17,18 @@ function Home() {
         {/* <Typography sx={{fontFamily:"fantasy"}} variant="h2" gutterBottom>
           This is a CRUD Application
         </Typography> */}
-       
-        {showBtn?(<Button variant='contained' sx={{marginTop:15,background:'orangered'}} onClick={handleClick}>
-        <Typography variant="h3"  >
-          View All Books
-        </Typography>
 
-        </Button>):(<Books/>)}
+        {showBtn ? (
+          <Button
+            variant="contained"
+            sx={{ marginTop: 15, background: "orangered" }}
+            onClick={handleClick}
+          >
+            <Typography variant="h3">View All Books</Typography>
+          </Button>
+        ) : (
+          <Books />
+        )}
       </Box>
     </div>
   );
