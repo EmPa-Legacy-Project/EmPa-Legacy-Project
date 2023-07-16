c699f5ca320c52b49c6e64283663a6e7afa342aa
Emre Guler <mfbg1985@gmail.com>
2023-07-16 23:15:54 +0300

    about section updated

--- client/src/components/Book/About.js
+++ client/src/components/Book/About.js
@@ -2,7 +2,7 @@ import React from "react";
 
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
-import Link from "@mui/material/Typography";
+import { Link } from "@mui/material";
 
 function About() {
   return (
@@ -15,26 +15,20 @@ function About() {
           By MERN STACK
         </Typography>
         <Typography variant="h4" gutterBottom sx={{ fontFamily: "fantasy" }}>
-          that is created by Melpomeni and Ameneh
-          <br></br>
-          and updated by{" "}
-          <a href="https://github.com/panos-mantis">
-            Panagiotis Mantis
-          </a> and <a href="https://github.com/guleremre">Emre Güler</a>
-          <Link
-            component="text"
-            variant="h3"
-            onClick={() => {
-              "https://github.com/panos-mantis";
-            }}
-            color="inherit"
-          >
-            Panagiotis Mantis
-          </Link>
-          <Link href="https://github.com/guleremre" color="inherit">
-            Emre Güler
-          </Link>
-          <Link href="#">Link</Link>
+          that is created by Melpomeni and Ameneh and updated by
+          <Typography variant="h4" sx={{ fontFamily: "fantasy" }}>
+            
+            <Link
+              href="https://github.com/panos-mantis"
+              color= "inherit"
+              underline="always"
+            >
+              Panagiotis Mantis
+            </Link> {" "}
+            <Link color= "inherit" href="https://github.com/guleremre" variant="inherit">
+              Emre Güler
+            </Link>
+          </Typography>
         </Typography>
       </Box>
     </div>
